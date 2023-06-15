838b8befbb4a9d1a83663ac36643d3e386026753
scriptdd <136689933+scriptdd@users.noreply.github.com>
2023-06-15 16:24:46 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-watch -n 0.1 aws dynamodb query --table-name loadtest --key-condition-expression "PK = :PK" --expression-attribute-values  '{":PK":{"S":"user1"}}'
+watch -n 0.1 aws dynamodb query --table-name $1 --key-condition-expression "PK = :PK" --expression-attribute-values  '{":PK":{"S":"user1"}}'
